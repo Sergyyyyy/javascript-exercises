f81e93fc609a0f9a5427866995cee4036c67015f
MrGio <penalosa.sergiobono@gmail.com>
2026-06-21 22:03:39 +0800

    Added solution for task

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let newStr = str.split("")
+    let reverse = newStr.reverse()
+    let join = reverse.join("")
 
+    return join
 };
 
 // Do not edit below this line
