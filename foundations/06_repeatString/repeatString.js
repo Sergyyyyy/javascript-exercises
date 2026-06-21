395d653ccbe9a9b0b3bcb94cc7f82e0c340c2524
MrGio <penalosa.sergiobono@gmail.com>
2026-06-21 21:48:48 +0800

    Added solution for task

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let stringArr = []
 
+    if (num < 0) { console.log('ERROR') }
+
+    for (let i = 0; i < num; i++){
+        const newStr = str;
+        stringArr.push(newStr)
+    }
+    let result = stringArr.join("")
+    return result
 };
 
 // Do not edit below this line
