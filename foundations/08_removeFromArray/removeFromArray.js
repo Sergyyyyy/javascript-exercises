97d836bdef82ef79c9cf4439b3fc7cb3623a1f7b
MrGio <penalosa.sergiobono@gmail.com>
2026-06-22 00:24:27 +0800

    Added solution for task

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+    // use filter to check the condition that if an item in an array is equal to the passed argument, we remove that item/don't include it.
+
+    //outerloop controls arr while inner loop controls ...nums!
+    for (let i = 0; i <= nums.length; i++) {
+        for (let j = 0; j <= arr.length; j++) {
+            if (arr[j] == nums[i]) {
+                arr.splice(j, 1)
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
