554947145dd16f6a4cdfcf5c9cbcf78087d561d8
MrGio <penalosa.sergiobono@gmail.com>
2026-06-22 22:19:03 +0800

    Added solution for task

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+
+// farhenheit to celsius
+const convertToCelsius = function(temp) {
+  // (32°F − 32) × 5/9 = 0°C
+  let celsius = (temp - 32) * 5/9
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+// celsius to farhenheit
+const convertToFahrenheit = function(temp) {
+  // (0°C × 9/5) + 32 = 32°F
+  let farhenheit = (temp * 9/5) + 32
+  return farhenheit
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
-};
+}; 
