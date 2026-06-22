d6c9ccc9db2b3da4455e71502e11f42a06109717
MrGio <penalosa.sergiobono@gmail.com>
2026-06-22 22:09:59 +0800

    Added solution for task.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // years divisible by 4 = leap years
+    // years divisible by 400 = leap years
+    // years divisible by 100 = not leap years
+    
+    if (year % 400 == 0) { return true }
 
+    if (year % 100 == 0) { return false }
+
+    if (year % 4 == 0) { return true}
+    else { return false }
 };
 
 // Do not edit below this line
