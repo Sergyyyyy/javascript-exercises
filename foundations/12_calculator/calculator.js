82a9c0690f3a4b231046475b4cdf8f787e08ac74
MrGio <penalosa.sergiobono@gmail.com>
2026-06-30 12:18:22 +0800

    Added solution as well as revised the JEST tests to avoid skipping.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0) { return 0 }
+  else { return arr.reduce((acc, cur) => acc + cur ) }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, cur) => acc * cur )
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    let holder = []
+
+    if (x == 0) { return 1}
+
+    for (let i = x; i >= 1; i--){
+        holder.push(i)
+        console.log(holder)
+    }
+    
+    return holder.reduce((acc, cur) => acc * cur)
 };
 
 // Do not edit below this line
