2d2d4af28dc2895cffaa53668cac0048a563cb2d
MrGio <penalosa.sergiobono@gmail.com>
2026-06-30 18:14:51 +0800

    Added solution for task.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // take string
+    // split string to arr
+    // reverse arr
+    // join arr = newString
+    // check if string = newString
+    // if == return true
+    // else return false
 
+    let stringArr = word.split("");
+    let reverse = stringArr.reverse()
+    let joined = reverse.join("")
+
+    if (joined != word) {
+        return false
+    }
+
+    return true
 };
 
 // Do not edit below this line
